28a4c0b709d9bb1faa34bfacf4478b226ea6122a
ramirodoes <ramirodoes@gmail.com>
2024-02-15 15:40:29 -0300

    armando preentrega

---
+++ ecommerce/src/components/navbar/navbar.jsx
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>La Escuelita de Sayago</h3>
+            <div>
+                <button>Camisetas</button>
+                <button>Shorts</button>
+                <button>Chombas</button>
+                <button>Camperas</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
